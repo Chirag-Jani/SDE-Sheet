66874d0d391df62d12a0cc6ce9931a9955febaa3
Chirag Jani <chirag.jani59001@gmail.com>
2022-06-05 19:46:35 +0530

    day 1 done - 3 array questions

---
+++ README.md
@@ -0,0 +1,5 @@
+Day 1
+
+- setZeroes
+- pascal's triangle
+- sort an array of 0's and 1's
---
+++ day 1/setZeroes.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+void setZeroes(vector<vector<int>> &matrix)
+{
+    int rows = matrix.size(), cols = matrix[0].size();
+    vector<int> rowDummy(rows, -1), colDummy(cols, -1);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            if (matrix[i][j] == 0)
+            {
+                rowDummy[i] = 0;
+                colDummy[j] = 0;
+            }
+        }
+    }
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            if (rowDummy[i] == 0 || colDummy[j] == 0)
+            {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+}
+
+int main()
+{
+    vector<vector<int>> arr;
+    arr = {{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}};
+    setZeroes(arr);
+    cout << "The Final Matrix is " << endl;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        for (int j = 0; j < arr[0].size(); j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << "\n";
+    }
+}
